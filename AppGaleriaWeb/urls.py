a31c8cf21135efb7cd9805b146bfd3553fdfef27
lutonhome2005 <lutonhome2005@gmail.com>
2023-10-02 02:00:58 -0300

    navbar login logout

--- AppGaleriaWeb/urls.py
+++ AppGaleriaWeb/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('login_view/', login_view, name='login_view'),
     path('register/', register, name='register'),
     path('logout/', LogoutView.as_view (template_name="logout.html"), name='logout'),
+    
 ]   
 
 
